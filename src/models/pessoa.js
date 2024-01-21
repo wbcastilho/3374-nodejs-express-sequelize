c44685852f7410e9a191deeb1a806e635fbf852a
wbcastilho <welber.castilho@yahoo.com.br>
2024-01-21 11:40:06 -0300

    Criadas migracoes e models Categoria, Curso e Matricula

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -3,14 +3,14 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pessoa extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class Pessoa extends Model {    
     static associate(models) {
-      // define association here
+      Pessoa.hasMany(models.Curso, {
+        foreignKey: 'docente_id'
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id'
+      });
     }
   }
   Pessoa.init({
