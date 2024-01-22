d4f44f851ec79544be3f8059a1bea827b183d477
wbcastilho <welber.castilho@yahoo.com.br>
2024-01-22 08:46:58 -0300

    Refatoracao codigo, concentrado pastas referente a ao banco de dados dentro de database

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,7 +8,7 @@ class Controller {
             const listaDeRegistros = await this.entidadeService.pegaTodosOsRegistros();
             return res.status(200).json(listaDeRegistros);
         } catch (erro) {
-            
+          return res.status(500).json({ erro: erro.message });
         }
     }
 
@@ -18,7 +18,7 @@ class Controller {
         const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
         return res.status(200).json(umRegistro);
       } catch (erro) {
-        // erro
+        return res.status(500).json({ erro: erro.message });
       }
     }
 
@@ -28,7 +28,7 @@ class Controller {
         const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
         return res.status(200).json(novoRegistroCriado);
       } catch (erro) {
-        // erro
+        return res.status(500).json({ erro: erro.message });
       }
     }
 
@@ -46,7 +46,7 @@ class Controller {
             return res.status(200).json({ mensagem: 'Atualizado com sucesso.'});
 
         } catch (erro) {
-
+          return res.status(500).json({ erro: erro.message });
         }
     }
 
@@ -57,8 +57,8 @@ class Controller {
         return res.status(200).json({ mensagem: `id ${id} deletado` });
 
 
-      } catch (error) {
-        return res.status(500).json(error.message);
+      } catch (erro) {
+        return res.status(500).json({ erro: erro.message });
       }
     }
 }
--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -15,7 +15,7 @@ class PessoaController extends Controller {
             const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
             return res.status(200).json(listaMatriculas);
         } catch (erro) {
-
+            return res.status(500).json({ erro: erro.message });
         }
     }
 }
