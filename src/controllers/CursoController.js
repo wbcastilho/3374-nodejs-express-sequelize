f145f1cf23827d94c404341be1dadb623f3100cb
wbcastilho <welber.castilho@yahoo.com.br>
2024-01-25 08:50:30 -0300

    Filtro de data inicial e data final ao consultar cursos

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
 
@@ -6,6 +8,26 @@ const cursoServices = new CursoServices();
 class CursoController extends Controller {
     constructor() {
         super(cursoServices);
+    }
+
+    async pegaCursos(req, res) {
+        // req.query pega as query strings enviadas
+        const { data_inicial, data_final } = req.query;
+        const where = {};
+
+        // if ternário - se existe os param, criar uma prop {}
+        data_inicial || data_final ? where.data_inicio = {} : null;
+        // if ternário - se existir data inicial, adiciona a prop gte com o valor
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+        // if ternário - se existir data final, idem
+        data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+        try {
+            const listaCursos = await cursoServices.pegaTodosOsRegistros(where);
+            return res.status(200).json(listaCursos);
+        } catch (erro) {
+            return res.status(500).json({ erro: erro.message });
+        }
     }
 }
 
